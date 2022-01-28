90414d14f8a50fe9bdbe307dc4cb1a617347af0d
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-01-28 20:29:37 -0300

    feat(58:00): create ListUserReceiveComplimentsService

---
+++ src/services/ListUserReceiverComplimentsService.ts
@@ -0,0 +1,19 @@
+import {getCustomRepository} from "typeorm";
+import {ComplimentsRepositories} from "../repositories/ComplimentsRepositories";
+
+
+class ListUserReceiverComplimentsService {
+
+  async execute(user_id: string) {
+    const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+
+    return await complimentsRepositories.find({
+      where: {
+        user_receiver: user_id
+      }
+    });
+  }
+  
+}
+
+export { ListUserReceiverComplimentsService};
