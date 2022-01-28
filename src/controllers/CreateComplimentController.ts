49a51686bfc81b2dca46e1e63b7f486c3e1131bd
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2022-01-28 19:58:14 -0300

    feat(47:30): refactor createComplimentController

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,19 +3,16 @@ import { CreateComplimentService } from '../services/CreateComplimentService';
 
 class CreateComplimentController {
     async handle(req: Request, res: Response) {
-      const {
-        tag_id,
-        user_receiver,
-        user_sender,
-        message
-      } = req.body;
+      const {tag_id, user_receiver, message} = req.body;
+
+      const {user_id} = req;
 
       const createCompliment = new CreateComplimentService();
 
       const compliment = await createCompliment.execute({
         tag_id,
         user_receiver,
-        user_sender,
+        user_sender: user_id,
         message
       });
 
